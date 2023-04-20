53f9cd7ccea077cbbe095473dbf9fd8e0d4f705d
Thiagogssx <thiagoguerreiro2015@gmail.com>
2023-04-19 21:02:37 -0400

    alteração para colocar valores diretamente de uma api

--- scripts.js
+++ scripts.js
@@ -1,14 +1,16 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.25
-const euro = 5.56
-const bitcoin = 0.0000068
-
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = 0.0000068
 
     realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
         style: 'currency', currency: 'BRL'
